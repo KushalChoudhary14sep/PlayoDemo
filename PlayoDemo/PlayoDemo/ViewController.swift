86d9698c93d1f8021523fc1be67538e349358fc7
kushal <kushal14sep@gmail.com>
2022-08-03 21:12:32 +0530

    project setup done

---
+++ PlayoDemo/PlayoDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayoDemo
+//
+//  Created by Kushal Choudhary on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
